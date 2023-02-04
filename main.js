4bfa6d557ae5e1348b98b2f202bd44c36daa2db5
Daisuke-Matsubara <110824363+Daisuke-Matsubara@users.noreply.github.com>
2023-02-04 17:43:39 +0900

    Initial commit

---
+++ main.js
@@ -0,0 +1,23 @@
+console.log("Hello World!!");
+console.log(65536);
+console.log(10+5);
+console.log(10*5);
+console.log("Hello" + "World!");
+//割り算の余りを求める場合は「％」を使用
+console.log(10%2);
+/*
+上記の場合、10割る2であまり0
+そのため出力は「0」
+下記の場合、出力は「1」になります
+*/
+console.log(10%3);
+
+/*JavaScript初級第1章課題
+問１
+*/
+console.log("10+8");
+//問2
+console.log(10+8);
+//問3
+console.log("20割る3の余りは" + 20%3 );
+window.alert("Hello World!!");
